ce19b798d68ba64440d261de2025ca5f37115e83
adamocallaghan <adam.ocallaghan@yahoo.ie>
2016-03-15 22:50:20 +0000

    Major changes to project.

--- mediasentiment.js
+++ mediasentiment.js
@@ -3,8 +3,8 @@ if (Meteor.isClient) {
   Session.setDefault('counter', 0);
 
   Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+    myHelper: function () {
+      return Session.get('myMethodResult');
     }
   });
 
@@ -13,7 +13,10 @@ if (Meteor.isClient) {
 		event.preventDefault();
 		var mySearchTerm = event.target.searchterm.value;
 		console.log("Hello client!" + mySearchTerm);
-		Meteor.call('sendLogMessage', mySearchTerm);
+		Meteor.call('sendLogMessage', mySearchTerm, function(error, result) {
+          Session.set('myMethodResult', result);
+            console.log(result);
+        });
     }
   });
 }
@@ -26,15 +29,34 @@ if (Meteor.isServer) {
   Meteor.methods({
     'sendLogMessage': function(mySearchTerm){
         console.log("Hello server!" + mySearchTerm);
-		//websiteData = Scrape.feed("http://www.rte.ie/search/?fq=pillar:%22News%22&query=sinn%20fein&fq=nolc_keywords:%22sinn%20f%C3%A9in%22");
 		websiteData = Scrape.feed("http://www.rte.ie/news/rss/news-headlines.xml");
-		//articleInfo = websiteData.items[1];
-		for (i=0; i<4; i++) {
+        returnThis = new Array();
+		for (i=0; i<9; i++) {
+            //console.log(websiteData.items.length);
 			articleTitle = websiteData.items[i].title;
 			articleLink = websiteData.items[i].link;
-			console.log(articleTitle);
-			console.log(articleLink);
+			//console.log(articleTitle);
+			//console.log(articleLink);
+            articleInfo = Scrape.website(articleLink);
+            //console.log(articleInfo.text);
+            var r1 = sentiment(articleInfo.text);
+            //console.log(r1);
+            //console.log(r1.score + " SENTIMENT SCORE");
+            //console.log(r1.comparative + " COMPARATIVE SCORE");
+            var positiveWords = r1.positive;
+            var negativeWords = r1.negative;
+            //console.log(positiveWords + " POSITIVE WORDS");
+            //console.log(negativeWords + " NEGATIVE WORDS");
+            if (r1.score > 0){
+                polarity = "pos";
+            } else {
+                polarity = "neg";
+            }
+            obj = new Object({score: r1.score, title: articleTitle, link: articleLink, comparative: r1.comparative, negWords: negativeWords, posWords: positiveWords, polarity: polarity, canvasId: i});
+            returnThis[i] = obj;
 		}
+        //Ok, figured it out, r1 is outside the loop so is only returning the last result in the array!
+      return returnThis;
     }
   });
 
